3f017144af5c27121a21f8ab9e6ecb598bb55686
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-07-31 18:45:05 +0100

    test for two sum passed and code is refactored

--- calculatorPackage/TwoSum.py
+++ calculatorPackage/TwoSum.py
@@ -1,15 +1,27 @@
-in_one, in_two = input("Enter a two number: ").split()
+# class TwoSum(object):
+#
+#    """
+#    For an example, suppose the array is like A = [2, 8, 12, 15],
+#    and the target sum is 20. Then it will return indices 1 and 2,
+#    as A[1] + A[2] = 20.
+#
+#    """
+   
+def two_Sum(number, target):
+   required_index = {}
+      
+   for index in range(len(number)):
+      if target - number[index] in required_index:
+         return [required_index[target - number[index]],index]
+      else:
+         required_index[number[index]]= index
 
-def two_sum(number, target)-> list[int]:
+# user_input_list = [2,2,12,15]
+# object1 = TwoSum()
+# print(object1.two_Sum(user_input_list, 27))
 
-# def two_sum(number, target):
-#     number= list[int]
-#     target= int
-    
-    for index in range(len(number)):
-        for second_index in range(index + 1, len(number)):
-            if number[index] + number[second_index] == target:
-                return index + second_index
-            
-
-    
+# a, b, c, d= input("Enter four number: ").split()
+# total= a, b, c, d
+#
+# classObject= TwoSum()
+# print(classObject.two_Sum(total,total))
