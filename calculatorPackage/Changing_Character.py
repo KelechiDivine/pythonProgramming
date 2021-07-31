843ddfc887e18d420de1714ca5804a96c2423a35
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-07-31 21:02:15 +0100

    changing character

---
+++ calculatorPackage/Changing_Character.py
@@ -0,0 +1,9 @@
+celebration= 'happy birthday'
+love_me_again= celebration.capitalize()
+print(love_me_again)
+
+_a_new_day_has_come= 'happy new year'
+new_year_celebration= _a_new_day_has_come.capitalize()
+another_new_year= _a_new_day_has_come.title()
+print(new_year_celebration)
+print(another_new_year)
