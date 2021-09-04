5b03e20b95a1de607df92dd946debbd9e37a262c
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-04 23:54:05 +0100

    created  a new mobile application

---
+++ PhoneDatabase/MobileApp.py
@@ -0,0 +1,40 @@
+class PhoneBook():
+    
+    def search(self):
+        pass
+    
+    def service_Nos(self):
+        pass
+    
+    def add_name(self, add_friend_name) -> str:
+        saved_name = add_friend_name
+        return saved_name
+    
+    def erase(self):
+        pass
+    
+    def edit(self):
+        pass
+    
+    def assign_tone(self):
+        pass
+    
+    def send_b_card(self):
+        pass
+    
+    class Option():
+        def __init__(self, type_of_view, memory_status):
+            self.type_of_view = type_of_view
+            self.memory_status = memory_status
+            
+        def option(self):
+            return self.memory_status, self.type_of_view
+        
+        
+    def speed_dial(self):
+        pass
+    
+    def voice_tag(self):
+        pass
+    
+    
---
+++ PhoneDatabase/MobileApp_Test.py
@@ -0,0 +1,11 @@
+import unittest
+from MobileApp import *
+
+class MyTestCase(unittest.TestCase):
+
+    def test_add_name_to_phoneBook(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
