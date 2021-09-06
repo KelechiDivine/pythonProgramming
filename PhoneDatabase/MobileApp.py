9fea4aeeaf1a8c3508140d00b252f72e99edb0be
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-06 11:46:00 +0100

    ...

--- PhoneDatabase/MobileApp.py
+++ PhoneDatabase/MobileApp.py
@@ -1,19 +1,63 @@
 import re
 
 class PhoneBook(object):
+   #
+   # def __init__(self, search, service_number, add_name, erase, edit,
+   #              assign_tones, send_b_card, options, speed_dial, voice_tag):
+   #
+   #     self.search = search
+   #     self.service_number = service_number
+   #     self.add_name = add_name
+   #     self.erase = erase
+   #     self.edit = edit
+   #     self.assign_tone = assign_tones
+   #     self.send_b_card = send_b_card
+   #     self.options = options
+   #     self.speed_dial = speed_dial
+   #     self.voice_tag = voice_tag
+   #
+   #
+   
+   def search(self):
+       print("1: Search")
+       
+   def service_number(self):
+       print("2: Service Number")
     
+   def add_name(self):
+        print("3: Add name")
     
-    def add_name(self, add_friends_name)->str :
-        names_in_phone_book = []
-        names_in_phone_book.append(add_friends_name)
-        print(names_in_phone_book)
-        return names_in_phone_book
-    
-    
-    search_name_in_phone_book = add_name("")
-    def search_method(self, search_name)->str:
-        phone_book = PhoneBook()
-        if search_name in re.search(phone_book.search_name_in_phone_book, search_name, []):
-            print("we found a name!")
-        else:
-            print("no name was found.")
+   def erase(self):
+       print("4: Erase")
+   
+   def edit(self):
+       print("5: Edit")
+
+   def assign_tone(self):
+       print("6: Assign tone")
+       
+   
+   def send_b_card(self):
+       print("7: Send b's card")
+       
+   
+   def option(self, is_navigate):
+       print("8: Options")
+
+       def navigate_options():
+           if is_navigate == 8:
+               print("\t-> Type Of view")
+               print("\t-> Memory status")
+
+       print(navigate_options())
+       
+   def speed_dials(self):
+       print("9: Speed Dials")
+       
+   def voice_tags(self):
+       print("10: Voice Tags")
+   
+   # def display_phone_book_menu(self, menu) -> str:
+   #     if menu == "Phone Book Menu":
+   #         return
+   #
--- PhoneDatabase/MobileApp_Test.py
+++ PhoneDatabase/MobileApp_Test.py
@@ -3,16 +3,64 @@ from MobileApp import PhoneBook
 
 class MyTestCase(unittest.TestCase):
 
-    def test_add_name_to_phoneBook(self):
+    def test_search(self):
         phoneBook = PhoneBook()
-        phoneBook.add_name("kelechi")
-        self.assertTrue("kelechi")
+        phoneBook.search()
+        self.assertTrue(True)
         
         
-    def test_search_by_name(self):
+    def test_service_number(self):
         new_phoneBook = PhoneBook()
-        new_phoneBook.search_method("Divine")
-        self.assertTrue("Divine")
+        new_phoneBook.service_number()
+        self.assertTrue(True)
+        
+        
+    def test_add_name(self):
+        phone_book = PhoneBook()
+        phone_book.add_name()
+        self.assertTrue(True)
+        
+        
+    def test_erase(self):
+        phoneBook = PhoneBook()
+        phoneBook.erase()
+        self.assertTrue(True)
+        
+        
+    def test_edit(self):
+        phone = PhoneBook()
+        phone.edit()
+        self.assertTrue(True)
+        
+        
+    def test_assign_tone(self):
+        phone = PhoneBook()
+        phone.assign_tone()
+        self.assertTrue(True)
+        
+        
+    def test_send_b_card(self):
+        phone = PhoneBook()
+        phone.send_b_card()
+        self.assertTrue(True)
+        
+        
+    def test_is_option(self):
+        phone_option = PhoneBook()
+        phone_option.option(8)
+        self.assertTrue(True)
+        
+        
+    def test_speed_dial(self):
+        phone = PhoneBook()
+        phone.speed_dials()
+        self.assertTrue(True)
+        
+        
+    def test_voice_log(self):
+        phone = PhoneBook()
+        phone.voice_tags()
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
