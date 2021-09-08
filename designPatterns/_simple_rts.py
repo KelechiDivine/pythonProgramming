7e908317d7a5539eabdc5c8ecfe952c847edf6af
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 15:03:06 +0100

    created a toString() that return all attributes of the class in a String format

--- designPatterns/_simple_rts.py
+++ designPatterns/_simple_rts.py
@@ -34,3 +34,19 @@ class Archer(object):
          self.attack_power = attack_power
          self.attack_range = attack_range
          self.weapon = weapon
+
+     def __str__(self):
+         return "Type: {0}\n" \
+                "Life: {1}\n" \
+                "Speed: {2}\n" \
+                "Attack Power: {3}\n" \
+                "Attack Range: {4}\n" \
+                "Weapon: {5}".format(
+             
+             self.unit_type,
+             self.life,
+             self.speed,
+             self.attack_power,
+             self.attack_range,
+             self.weapon)
+     
