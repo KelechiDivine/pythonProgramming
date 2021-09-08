2c65b78bdb71172cda97b7b2a0016ca8ae403912
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 12:20:13 +0100

    created logger class

--- designPatterns/Logger2.py
+++ designPatterns/Logger2.py
@@ -1,20 +1,18 @@
-def write_log(level, msg):
-    with open("/var/log/filename.log", "a") as log_file:
+def write_log(filename, level, msg):
+    with open(filename, "a") as log_file:
         log_file.write("[{0}] {1}\n".format(level, msg))
-
+        
 def critical(msg):
     write_log("CRITICAL",msg)
-
+ 
 def error(msg):
     write_log("ERROR", msg)
-
+ 
 def warn(msg):
     write_log("WARN", msg)
 
-
 def info(msg):
     write_log("INFO", msg)
-
-
+ 
 def debug(msg):
     write_log("DEBUG", msg)
---
+++ designPatterns/Logger_Class.py
@@ -0,0 +1,6 @@
+class Logger(object):
+    """A file-based message logger with the following properties
+     Attributes:
+     file_name: a string representing the full path of the log file to which
+    this logger will write its messages
+     """
