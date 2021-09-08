c834f9540334730062fb0db21abad889ee64e2a1
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 15:42:55 +0100

    created an API variable to hold the API response

---
+++ crashCourse/python_repos.py
@@ -0,0 +1,12 @@
+import requests
+
+# make an API call and store the response
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers= headers)
+print(f"Status code: {r.status_code}")
+
+# store API response in a variable.
+
+response_dict = r.json()
