03a006a238368d4e74b718e2eb2fae6c2a977410
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 12:32:44 +0100

    class object

--- designPatterns/Logger_Class.py
+++ designPatterns/Logger_Class.py
@@ -4,3 +4,28 @@ class Logger(object):
      file_name: a string representing the full path of the log file to which
     this logger will write its messages
      """
+    
+    def __init__(self, file_name):
+        """Return a Logger object whose file_name is *file_name*"""
+        self.file_name = file_name
+        
+    def write_log(self, level, msg):
+        """Writes a message to the file_name for a specific Logger instance"""
+        with open(self.file_name, "a") as log_file:
+            log_file.write("[{0}] {1}\n".format(level, msg))
+            
+
+    def critical(self, level, msg):
+        self._write_log("CRITICAL", msg)
+        
+def error(self, level, msg):
+    self._write_log("ERROR", msg)
+    
+def warn(self, level, msg):
+    self._write_log("WARN", msg)
+
+def info(self, level, msg):
+    self._write_log("INFO", msg)
+
+def debug(self, level, msg):
+    self._write_log("DEBUG", msg)
