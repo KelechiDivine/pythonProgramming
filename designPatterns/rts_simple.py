ff75a319232418dd25cde3cce00491f71c557058
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 14:41:06 +0100

    created Knight() constructors and attributes

---
+++ designPatterns/rts_simple.py
@@ -0,0 +1,11 @@
+class Knight(object):
+    def __init__(self, life, speed,
+                 attack_power, attack_range,
+                 weapon):
+        
+        self.life = life
+        self.speed = speed
+        self.attack_power = attack_power
+        self.attack_range = attack_range
+        self.weapon = weapon
+        
