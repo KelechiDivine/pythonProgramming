e977af28a61de57a10128be13850ef3de993e353
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 14:59:11 +0100

    created a archer class

--- designPatterns/_simple_rts.py
+++ designPatterns/_simple_rts.py
@@ -23,3 +23,14 @@ class Knight(object):
             self.attack_range,
             self.weapon)
             
+class Archer(object):
+     def __init__(self, life, speed, attack_power,
+         attack_range,
+         weapon):
+         
+         self.unit_type = "Archer"
+         self.life = life
+         self.speed = speed
+         self.attack_power = attack_power
+         self.attack_range = attack_range
+         self.weapon = weapon
