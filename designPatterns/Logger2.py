0edd5529dddc3bfabd958b9d06576ad5097beef3
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 14:20:53 +0100

    cleaning up

--- designPatterns/Logger2.py
+++ designPatterns/Logger2.py
@@ -1,18 +1,19 @@
-def write_log(filename, level, msg):
-    with open(filename, "a") as log_file:
-        log_file.write("[{0}] {1}\n".format(level, msg))
+class Logger(object):
+    def write_log(filename, level, msg):
+        with open(filename, "a") as log_file:
+            log_file.write("[{0}] {1}\n".format(level, msg))
         
-def critical(msg):
-    write_log("CRITICAL",msg)
+    def critical(msg):
+        write_log("CRITICAL",msg)
+     
+    def error(msg):
+        write_log("ERROR", msg)
  
-def error(msg):
-    write_log("ERROR", msg)
- 
-def warn(msg):
-    write_log("WARN", msg)
+    def warn(msg):
+        write_log("WARN", msg)
 
-def info(msg):
-    write_log("INFO", msg)
+    def info(msg):
+        write_log("INFO", msg)
  
-def debug(msg):
-    write_log("DEBUG", msg)
+    def debug(msg):
+        write_log("DEBUG", msg)
---
+++ designPatterns/new_script_with_logger.py
@@ -0,0 +1,4 @@
+import Logger2
+
+logger_object = Logger2("/var/log/class_logger.log")
+# logger_object
