8b293755483640e7e17b8a8b2d55aa02b1f8b66d
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-08 14:35:19 +0100

    printed all Singleton auto generated numbers or id

--- designPatterns/test_singleton.py
+++ designPatterns/test_singleton.py
@@ -4,3 +4,12 @@ obj1 = SingletonObject()
 
 obj1.val = "Object value 1"
 print("print obj1: ", obj1)
+
+print("=====")
+
+obj2 = SingletonObject()
+obj2.val = "Object value 2"
+
+print("print obj1: ", obj1)
+print("print obj2: ", obj2)
+
