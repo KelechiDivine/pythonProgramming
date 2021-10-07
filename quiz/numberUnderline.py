37bdbe28aadcf24d37d0345f5f04f0053e7b6e8a
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-10-07 13:57:42 +0100

    forloop in html

---
+++ quiz/numberUnderline.py
@@ -0,0 +1,28 @@
+# for x in range(0, 10):
+#    mod3 = x % 3
+#    x % 3 == 0
+#
+#    mod10 = x % 10
+#    x % 10 == 0
+#
+#    mod_var = ''
+#
+#    if mod3:
+#       mod_var = f'<p style="color: red;"><u>{x}</u></p>'
+#
+#    if mod10:
+#       mod_var = f'<p style="color: blue;"><u>{x}</u></p>'
+#
+#    # if not mod3 or mod10:
+#    #    mod_var = f'<p style="color: purple;"><em>{x}</em></p>'
+#
+#    print(mod_var)
+#
+
+
+# html = """<b><u><i></b></u></i>"""
+#
+
+f = open('temp.html','w')
+f.write(html)
+f.close()
