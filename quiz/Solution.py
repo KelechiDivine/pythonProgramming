af2f2b9002cc8d1856b4c86292a3f25500e58eae
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-10-07 13:22:36 +0100

    new fizz, buzz, fizzbuzz commit

---
+++ quiz/Solution.py
@@ -0,0 +1,12 @@
+for x in range(1, 101):
+   if x % 15 == 0:
+      print(f"{x} is a FizzBuzz...")
+   
+   elif x % 3 == 0:
+      print(f"{x} is a Buzz...")
+   
+   elif x % 5 == 0:
+      print(f"{x} is a Fizz...")
+   
+   else:
+      print(x)
