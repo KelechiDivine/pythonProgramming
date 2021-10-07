712e6b04194aa403d30e81c7b2b34689cb118196
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-10-07 13:23:27 +0100

    Uncommented my bincom test class

--- quiz/test_bincomInterview.py
+++ quiz/test_bincomInterview.py
@@ -1,27 +1,27 @@
-# import unittest
-# from bincomInterview import *
-#
-# class BincomInterviewTest(unittest.TestCase):
-#    def test_mutiples_of_3(self):
-#       mutilple_of_3 = BincomInterview()
-#       x = mutilple_of_3.multiples_of_number(3)
-#       self.assertEquals(3, x)
-#
-#    def test_mutiples_of_5(self):
-#       multiple_of_5 = BincomInterview()
-#       x = multiple_of_5.multiples_of_number(5)
-#       self.assertEquals(5, x)
-#
-#    def test_multiples_of_15(self):
-#       multiples_of_15 = BincomInterview()
-#       x = multiples_of_15.multiples_of_number(15)
-#       self.assertEquals(15, x)
-#
-#    def test_assertFail(self):
-#       it_will_fail_if_number_is_invalid = BincomInterview()
-#       x = it_will_fail_if_number_is_invalid.multiples_of_number(122334)
-#       self.assertFail()
-#
-#
-# if __name__ == '__main__':
-#    unittest.main()
+import unittest
+from bincomInterview import *
+
+class BincomInterviewTest(unittest.TestCase):
+   def test_mutiples_of_3(self):
+      mutilple_of_3 = BincomInterview()
+      x = mutilple_of_3.multiples_of_number(3)
+      self.assertEquals(3, x)
+      
+   def test_mutiples_of_5(self):
+      multiple_of_5 = BincomInterview()
+      x = multiple_of_5.multiples_of_number(5)
+      self.assertEquals(5, x)
+      
+   def test_multiples_of_15(self):
+      multiples_of_15 = BincomInterview()
+      x = multiples_of_15.multiples_of_number(15)
+      self.assertEquals(15, x)
+      
+   def test_assertFail(self):
+      it_will_fail_if_number_is_invalid = BincomInterview()
+      x = it_will_fail_if_number_is_invalid.multiples_of_number(122334)
+      self.assertFail()
+
+
+if __name__ == '__main__':
+   unittest.main()
