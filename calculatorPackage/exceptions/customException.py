ca6f7c25ff0dacd7e56154b873d4a6c03f13f35f
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-11-01 10:44:42 +0100

    new custom error creation

--- calculatorPackage/exceptions/customException.py
+++ calculatorPackage/exceptions/customException.py
@@ -1,7 +1,32 @@
-class CustomError(Exception):
+class Error(Exception):
    pass
 
-raise CustomError
+class ValueTooSmallError(Error):
+   pass
 
-__main__.CustomError
-raise CustomeError("An error occured...")
+class ValueTooLargeError(Error):
+   pass
+
+
+number = 10
+
+
+while True:
+   try:
+      i_num = int(input("Enter a number: "))
+      if i_num < number:
+         raise ValueTooSmallError
+      
+      elif i_num > number:
+         raise ValueTooLargeError
+      break
+      
+   except ValueTooSmallError:
+      print("This value is too small, try again!!!")
+      print()
+      
+   except ValueTooLargeError:
+      print("This value is too large, try again!!!")
+      print()
+      
+print("Congratulations! You guessed correctly...")
