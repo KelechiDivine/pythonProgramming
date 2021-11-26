b2f8a81759db1c2439801934ccf8d1c28848a858
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-11-26 16:59:29 +0100

    Created sales by match class

--- ChapterFour/test_turing.py
+++ ChapterFour/test_turing.py
@@ -1,17 +1,19 @@
 import unittest
 from turing import *
 
+
 class TuringTest(unittest.TestCase):
-    
+
     def test_turing_polls_exists(self):
         new_turing = Turing()
         self.assertIsNotNone(new_turing)
-        
+
     def test_calPoint_will_execute(self):
         turing = Turing()
         myList = ["5", "2", "C", "D", "+"]
-        
+
         self.assertEqual(turing.calPoint(myList), 30)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
---
+++ hackerRank/SalesByMatch.py
@@ -0,0 +1,3 @@
+
+"""The goal is to count the numbers of how many pairs of socks. This also gives info that the final result should be
+an integer. """
